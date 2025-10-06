897fcbb1a7adda55ad355c2899ba51775b40679a
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-10-06 16:11:20 +0200

    add posts client

---
+++ api_client/clients/posts_client.py
@@ -0,0 +1,44 @@
+from api_client.base_client import BaseClient
+from models.post_models import CreatePostRequest
+import requests
+
+
+class PostsClient:
+    ENDPOINT = "/posts"
+
+    def __init__(self, base_client: BaseClient):
+        self.client = base_client
+
+    def get_all_posts(self) -> requests.Response:
+        """GET /posts"""
+        return self.client.get(self.ENDPOINT)
+
+    def get_post_by_id(self, post_id: int) -> requests.Response:
+        """GET /posts/{id}"""
+        path = f"{self.ENDPOINT}/{post_id}"
+        return self.client.get(path)
+
+    def create_post(self, title: str, body: str, userId: int = 1) -> requests.Response:
+        """POST /posts"""
+        post_data = CreatePostRequest(title=title, body=body, userId=userId)
+
+        return self.client.post(
+            path=self.ENDPOINT,
+            json=post_data.to_dict()
+        )
+
+    def update_post(self, post_id: int, title: str, body: str, userId: int) -> requests.Response:
+        """PUT /posts/{id}"""
+        path = f"{self.ENDPOINT}/{post_id}"
+        update_data = {
+            "id": post_id,  # JSONPlaceholder required ID in body
+            "title": title,
+            "body": body,
+            "userId": userId
+        }
+        return self.client.put(path, json=update_data)
+
+    def delete_post(self, post_id: int) -> requests.Response:
+        """DELETE /posts/{id}"""
+        path = f"{self.ENDPOINT}/{post_id}"
+        return self.client.delete(path)
